d165ba97052169879a02fd84b6a3e3a24d453e2d
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-25 19:19:01 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+# You can customise your welcome message while deploying this bot 
+# All Copyright © goes to SaminSumesh
+# GNU LICENSE V3 Under Public Source
+
+
+
+import os
+from pyrogram import Client, filters
+
+Bot = Client(
+"Simple Welcome Bot",
+bot_token=os.environ.get("BOT_TOKEN"),
+api_id=int(os.environ.get("API_ID")),
+api_hash=os.environ.get("API_HASH"),
+group=os.environ.get("GROUP"),
+welcome_message=os.environ.get("WELCOME_MESSAGE")
+)
+
+
+# This just an pyrogram welcome message bot that can be used in your groups.
+
+@Bot.on_message(filters.chat(GROUP) & filters.new_chat_members)
+def welcomebot(bot, message):
+    message.reply_text(WELCOM_MESSAGE)
+    
+print("Bot is now running, If any issues contact us through @XD_Botzsupport")
+
+Bot.run()
